840511a5984fbe33e09132f9156cf1ad51ee4136
nikita nimore <992nikitanimore@gmail.com>
2024-10-02 00:11:54 +0530

    intoduction of function and its first type Named function

--- 03functions.js
+++ 03functions.js
@@ -1,8 +1,26 @@
+// function - 
+// In JavaScript, a function is a block of code designed to 
+// perform a specific task. Functions allow you to reuse code, 
+// making programs more modular and efficient.
 
 
+// function 8 type ke hote  hai 
 
-function isEven(num){
-    return num%2===0;
+//  1. Function Declaration (Named Function) 
+
+//    यह functions को एक नाम देकर घोषित किया जाता है और code में कहीं भी उपयोग किया जा सकता है
+// आप function को declare करने से पहले call कर सकते हैं।
+// example - 1
+greet();
+
+function greet() {
+    console.log("Hello!");
+}
+
+// example - 2
+
+function isEven(num) {
+    return num % 2 === 0;
 }
 
 console.log(isEven(24));
